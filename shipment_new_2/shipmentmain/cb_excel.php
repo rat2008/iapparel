ee0a30c6110ae3764d6ca52566fecd4c06e3ff18
yikmun <yk.wong@tio.asia>
2025-04-10 12:34:23 +0800

    complete excel

--- shipment_new_2/shipmentmain/cb_excel.php
+++ shipment_new_2/shipmentmain/cb_excel.php
@@ -25,6 +25,14 @@ $style_center = array(
     )
 );
 
+$border_allborders = array(
+    'borders' => array(
+        'allborders' => array(
+            'style' => PHPExcel_Style_Border::BORDER_THIN
+        )
+    )
+);
+
 $cost_heads = $model_cost_head->getAllByArr(['invID' => $invID]);
 
 // Initialize PHPExcel
@@ -39,12 +47,14 @@ $sheet->getProperties()
     ->setDescription("Exported cost breakdown data");
 
 // Add headers
-$activeSheet->setCellValue('A1', 'Cost and Weight breakdown');
+$activeSheet->setCellValue('A1', 'Inv Attachment');
+$activeSheet->setCellValue('A2', 'Cost and Weight breakdown');
 $sheet->getActiveSheet()->mergeCells('A1:G1');
-$sheet->getActiveSheet()->getStyle('A1')->applyFromArray($style_center);
-$activeSheet->getStyle('A1')->getFont()->setBold(true);
+$sheet->getActiveSheet()->mergeCells('A2:G2');
+$sheet->getActiveSheet()->getStyle('A1:A2')->applyFromArray($style_center);
+$activeSheet->getStyle('A1:A2')->getFont()->setBold(true);
 
-$row = 2;
+$row = 3;
 foreach ($row_buyer_po as $buyer_po) {
     $activeSheet->setCellValue("A$row", 'PO#')
         ->setCellValue("B$row", $buyer_po['GTN_buyerpo']);
@@ -74,13 +84,15 @@ foreach ($row_buyer_po as $buyer_po) {
 
         $activeSheet->setCellValue("A$row", 'ITEM DESCRIPTION')
             ->setCellValue('B' . $row, $cost_head['item_desc']);
-            $sheet->getActiveSheet()->mergeCells('B' . $row . ':G' . $row);
+        $sheet->getActiveSheet()->mergeCells('B' . $row . ':G' . $row);
 
         $row++;
 
         $activeSheet->setCellValue('A' . ($row), 'Color')
             ->setCellValue('B' . ($row), implode(', ', $color_arr));
-            $sheet->getActiveSheet()->mergeCells('B' . $row . ':C' . $row);
+        $sheet->getActiveSheet()->mergeCells('B' . $row . ':C' . $row);
+        $activeSheet->getStyle('B' . ($row))->getAlignment()->setWrapText(true);
+        $activeSheet->getColumnDimension('B')->setWidth(40);
 
         $row++;
 
@@ -91,9 +103,15 @@ foreach ($row_buyer_po as $buyer_po) {
             ->setCellValue('F' . $row, "NNW /CTNS \n ( KG)")
             ->setCellValue('G' . $row, "TOTAL NNW \n (KG)");
 
-        $sheet->getActiveSheet()->getStyle('E'. $row)->getAlignment()->setWrapText(true);
-        $sheet->getActiveSheet()->getStyle('F'. $row)->getAlignment()->setWrapText(true);
-        $sheet->getActiveSheet()->getStyle('G'. $row)->getAlignment()->setWrapText(true);
+        $sheet->getActiveSheet()->getStyle('A' . $row . ':G' . $row)->applyFromArray($border_allborders);
+
+        $sheet->getActiveSheet()->getStyle('B' . $row)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+        $sheet->getActiveSheet()->getStyle('D' . $row)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+        $sheet->getActiveSheet()->getStyle('B' . $row)->getAlignment()->setWrapText(true);
+        $sheet->getActiveSheet()->getStyle('E' . $row)->getAlignment()->setWrapText(true);
+        $sheet->getActiveSheet()->getStyle('F' . $row)->getAlignment()->setWrapText(true);
+        $sheet->getActiveSheet()->getStyle('G' . $row)->getAlignment()->setWrapText(true);
+        $sheet->getActiveSheet()->getStyle('B' . $row . ':G' . $row)->applyFromArray($style_center);
 
         $row++;
 
@@ -101,25 +119,32 @@ foreach ($row_buyer_po as $buyer_po) {
 
         foreach ($row_cost_detail as $cost_detail) {
             $qty = $cost_detail['qty'];
-            $amount = $cost_detail['qty'] * $cost_detail['unitprice'];
+            (double)$amount = (double)$cost_detail['qty'] * (double)$cost_detail['unitprice'];
             $total_amount = $total_amount + $amount;
             $final_total_nnw = $final_total_nnw + $cost_detail['total_nnw'];
 
             $activeSheet->setCellValue('A' . $row, $cost_detail['item_desc'])
                 ->setCellValue('B' . $row, $cost_detail['qty'])
                 ->setCellValue('C' . $row, 'PCS')
-                ->setCellValue('D' . $row, '$'.$cost_detail['unitprice'])
-                ->setCellValue('E' . $row, '$'.$amount)
-                ->setCellValue('F' . $row, '$'.$cost_detail['ctn_qty'])
-                ->setCellValue('G' . $row, '$'.$cost_detail['total_nnw']);
+                ->setCellValue('D' . $row, '$' . $cost_detail['unitprice'])
+                ->setCellValue('E' . $row, '$' . $amount)
+                ->setCellValue('F' . $row, '$' . $cost_detail['ctn_qty'])
+                ->setCellValue('G' . $row, '$' . $cost_detail['total_nnw']);
+
+            $sheet->getActiveSheet()->getStyle('B' . $row . ':G' . $row)->applyFromArray($style_center);
+            $sheet->getActiveSheet()->getStyle('A' . $row . ':G' . $row)->applyFromArray($border_allborders);
 
             $row++;
         }
         $activeSheet->setCellValue('A' . $row, 'Total Amount:')
-                ->setCellValue('B' . $row, $qty)
-                ->setCellValue('C' . $row, 'SETS')
-                ->setCellValue('E' . $row, '$'.$total_amount)
-                ->setCellValue('G' . $row, $final_total_nnw);
+            ->setCellValue('B' . $row, $qty)
+            ->setCellValue('C' . $row, 'SETS')
+            ->setCellValue('E' . $row, '$' . $total_amount)
+            ->setCellValue('G' . $row, $final_total_nnw);
+
+        $sheet->getActiveSheet()->getStyle('A' . $row . ':G' . $row)->applyFromArray($style_center);
+
+        $sheet->getActiveSheet()->getStyle('A' . $row . ':G' . $row)->applyFromArray($border_allborders);
 
         $activeSheet->getStyle('A' . $row)->getFont()->setBold(true);
         $activeSheet->getStyle('B' . $row)->getFont()->setBold(true);
@@ -131,37 +156,6 @@ foreach ($row_buyer_po as $buyer_po) {
     }
     $row++;
 }
-// $activeSheet->setCellValue('A2', 'Cost and Weight breakdown');
-// $activeSheet->setCellValue('B1', 'ITEM/STYLE#')
-//     ->setCellValue('C1', 'ITEM DESCRIPTION')
-//     ->setCellValue('D1', 'COLOR')
-//     ->setCellValue('E1', 'QTY')
-//     ->setCellValue('F1', 'UNIT PRICE')
-//     ->setCellValue('G1', 'TOTAL AMOUNT')
-//     ->setCellValue('H1', 'NNW / CTNS (KG)')
-//     ->setCellValue('I1', 'TOTAL NNW (KG)');
-
-// // Style headers
-// $activeSheet->getStyle('A1:I1')->getFont()->setBold(true);
-
-// // Populate data
-// $row = 2; // Start from the second row
-// foreach ($cost_heads['row'] as $cost_head) {
-//     $cost_details = $model_cost_detail->getAllByArr(['INVCHID' => $cost_head['INVCHID']]);
-
-//     foreach ($cost_details['row'] as $detail) {
-//         $activeSheet->setCellValue("A$row", $cost_head['shipmentpriceID'])
-//             ->setCellValue("B$row", $cost_head['item_desc'])
-//             ->setCellValue("C$row", $detail['item_desc'])
-//             ->setCellValue("D$row", $cost_head['colorID'])
-//             ->setCellValue("E$row", $detail['qty'])
-//             ->setCellValue("F$row", $detail['unitprice'])
-//             ->setCellValue("G$row", $detail['qty'] * $detail['unitprice']) // Total Amount
-//             ->setCellValue("H$row", $detail['ctn_qty'])
-//             ->setCellValue("I$row", $detail['total_nnw']);
-//         $row++;
-//     }
-// }
 
 // Auto-size columns
 foreach (range('A', 'I') as $columnID) {
@@ -169,9 +163,9 @@ foreach (range('A', 'I') as $columnID) {
 }
 
 // Clear output buffer to prevent unexpected output
-if (ob_get_length()) {
-    ob_end_clean();
-}
+// if (ob_get_length()) {
+//     ob_end_clean();
+// }
 
 // Output to browser
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
